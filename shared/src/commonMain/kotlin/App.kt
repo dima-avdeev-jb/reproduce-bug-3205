33f0abb0bccf71494f999e0a19adf930bdc9b9f4
dima.avdeev <dima.avdeev@jetbrains.com>
2023-05-26 10:17:36 +0400

    reproduced

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,19 +1,5 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun App() {
@@ -24,12 +10,11 @@ fun App() {
     }
 }
 
-object MaterialUI2 : FullStyle2, MAUIMetadata2
+object MaterialUI2 : RedundantInterface, MAUIMetadata2 // Order maters
 
-interface FullStyle2: InterfaceInAnotherModule
+interface RedundantInterface : InterfaceInAnotherModule // Bug here
 
 interface MAUIMetadata2 : InterfaceInAnotherModule {
-
     @Composable
     override fun SomeUI(
         content: @Composable () -> Unit,
